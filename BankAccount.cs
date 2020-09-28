dd55a222ea83b8e5e666107cf335a14d1f5262d9
Simeon Bukurov <71982116+SwagerStorm@users.noreply.github.com>
2020-09-28 22:59:55 +0300

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class BankAccount
+    {
+		List<double> DepositList = new List<double>();
+		public string username { get; set; }
+
+		public double Balance { get; set; }
+
+		public BankAccount(string username, double Balance)
+		{
+			this.username = username;
+			this.Balance = Balance;
+
+			Console.WriteLine("Username: {0} , Balance: {1}", username, Balance);
+			NextMenu();
+		}
+
+		public void NextMenu()
+		{
+			double balance = Balance;
+			Console.WriteLine("Enter 1 for Withdrawl, 2 for Deposit, 3 for Logout");
+			string StringMenu = Console.ReadLine();
+			int NextChoice = Convert.ToInt32(StringMenu);
+			switch (NextChoice)
+			{
+				case 1:
+					Withdraw();
+					break;
+				case 2:
+					Deposit();
+					break;
+				case 3:
+					Logout();
+					break;
+			}
+		}
+
+		public double Withdraw()
+		{
+			Console.WriteLine("Available Balance: {0}", Balance);
+			Console.WriteLine("How much would you like to withdraw?: ");
+			double WithdrawAmount = Convert.ToDouble(Console.ReadLine());
+			Balance = Balance -= WithdrawAmount;
+			Console.WriteLine(Balance);
+			NextMenu();
+			return WithdrawAmount;
+		}
+
+		public double Deposit()
+		{
+			Console.WriteLine("Available Balance: {0}", Balance);
+			Console.WriteLine("How much would you like to deposit?: ");
+			double DepositAmount = Convert.ToDouble(Console.ReadLine());
+
+
+			DepositList.Add(DepositAmount);
+
+			Balance = Balance + DepositAmount;
+			Console.WriteLine(Balance);
+			NextMenu();
+			return DepositAmount;
+		}
+
+		public void Logout()
+		{
+			Console.WriteLine("Goodbye: " + username);
+		}
+
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = " ";
+            Console.WriteLine("Enter username: ");
+            username = Console.ReadLine();
+            BankAccount newAccount = new BankAccount(username, 200);
+        }
+    }
+}
